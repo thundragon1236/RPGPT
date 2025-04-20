42029e1fcc5208c3ceae4cdb80ea5de425bd4d4f
Vishwajit <vishwajit1236@gmail.com>
2025-04-20 17:06:31 +0530

    1.5

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ DATA_DIR = "data"
 # For AI integration (replace with real values or use env vars in production)
 OR_API_KEY = os.getenv("OR_API_KEY", "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")
 OR_MODEL = os.getenv("OR_MODEL", "google/gemini-2.5-flash-preview")
+OR_ENDPOINT = os.getenv("OR_ENDPOINT", "https://openrouter.ai/api/v1/chat")
 
